23fa9f6087f427f6bc5c7523a3b19e00fe08a6d3
Nick Ooms <nick.ooms@virtuosolar.com>
2020-10-24 20:02:07 +0200

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
+const BASE_URL = 'https://api.figma.com/v1';
 const FIGMA_TOKEN = '131048-174ca94f-2b4b-43df-b441-5d6cc84793bb';
 const AVASCO_FILE_KEY = 'YnKVSZa6n17N9ISVjD3ZAT';
 
-class API {
+/* class API {
   constructor(baseURL, operations) {
     this._baseURL = baseURL;
     this._operations = operations;
     Object.assign(
       this,
-      Object.keys(operations).map((operationName) => async (...args) => {
-        const result = await this.operation(operationName, ...args);
-        return result;
-      })
+      Object.fromEntries(
+        Object.keys(operations).map((operationName) => [
+          operationName,
+          this.makeCaller(operationName),
+        ])
+      )
     );
   }
 
+  makeCaller = (operationName) => async (...args) => {
+    const result = await this.operation(operationName, ...args);
+    return result;
+  };
+
   async operation(operationName, ...params) {
-    const url = `${this.baseURL}/${operationName}/${params.join('/')}`;
+    const url = `${this.baseURL}}/${params.join('/')}`;
     const headers = {
       'X-Figma-Token': FIGMA_TOKEN,
     };
@@ -28,11 +36,22 @@ class API {
     const json = await response.json();
     return json;
   }
-}
+} */
 
-const api = new API('https://api.figma.com/v1', {
+const operation = async (...params) => {
+  const url = `${BASE_URL}/${params.join('/')}`;
+  const headers = {
+    'X-Figma-Token': FIGMA_TOKEN,
+  };
+  const options = { headers };
+  const response = await fetch(url, options);
+  const json = await response.json();
+  return json;
+};
+
+/* const api = new API(BASE_URL, {
   files: ['key'],
-});
+}); */
 // const user = 'https://api.figma.com/v1/me';
 // const file = `https://api.figma.com/v1/files/${AVASCO_FILE_KEY}`;
 // const projects = 'https://api.figma.com/v1/teams/:team_id/projects';
@@ -45,7 +64,7 @@ function App() {
     return result;
   }; */
   useEffect(() => {
-    api.files(AVASCO_FILE_KEY).then((file) => {
+    operation('files', AVASCO_FILE_KEY).then((file) => {
       const canvas = file.document.children;
       console.log(canvas.map((c) => c.name).join('\n'));
       setCanvas(canvas);
