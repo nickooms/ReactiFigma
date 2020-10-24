8eb5e1af10f5f6637db2135f4f9602260db57ea9
Nick Ooms <nick.ooms@virtuosolar.com>
2020-10-24 17:22:15 +0200

    API class

--- src/App.js
+++ src/App.js
@@ -2,20 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 
+const FIGMA_TOKEN = '131048-174ca94f-2b4b-43df-b441-5d6cc84793bb';
 const AVASCO_FILE_KEY = 'YnKVSZa6n17N9ISVjD3ZAT';
 
+class API {
+  constructor(baseURL, operations) {
+    this._baseURL = baseURL;
+    this._operations = operations;
+    Object.assign(
+      this,
+      operations.map((operationName) => async (...args) => {
+        const result = await this.operation(operationName, ...args);
+        return result;
+      })
+    );
+  }
+
+  async operation(operationName, ...params) {
+    const url = `${this.baseURL}/${operationName}/${params.join('/')}`;
+    const headers = {
+      'X-Figma-Token': FIGMA_TOKEN,
+    };
+    const options = { headers };
+    const response = await fetch(url, options);
+    const json = await response.json();
+    return json;
+  }
+}
+
+const api = new API('https://api.figma.com/v1', {
+  files: { key: undefined },
+});
 // const user = 'https://api.figma.com/v1/me';
-const file = `https://api.figma.com/v1/files/${AVASCO_FILE_KEY}`;
+// const file = `https://api.figma.com/v1/files/${AVASCO_FILE_KEY}`;
 // const projects = 'https://api.figma.com/v1/teams/:team_id/projects';
 
 function App() {
   const login = async () => {
-    const response = await fetch(file, {
-      headers: {
-        'X-Figma-Token': '131048-174ca94f-2b4b-43df-b441-5d6cc84793bb',
-      },
-    });
-    console.log(await response.json());
+    const result = await api.files(AVASCO_FILE_KEY);
+    console.log(result);
   };
   useEffect(() => {
     login();
